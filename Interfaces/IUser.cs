cb2b0dae47332a7c3b9d02dbe18c71af61b93552
emeliehensfelt <emelie046@gmail.com>
2023-10-23 15:05:58 +0200

    Initial commit

---
+++ Interfaces/IUser.cs
@@ -0,0 +1,14 @@
+﻿using TravelPal_Newton.Enums;
+
+namespace TravelPal_Newton.Interfaces
+{
+    public interface IUser
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public Country Location { get; set; }
+
+
+    }
+
+}
---
+++ Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using TravelPal_Newton.Interfaces;
+
+namespace TravelPal_Newton.Models
+{
+    public class User : IUser
+    {
+        public required string Username { get; set; }
+        public required string Password { get; set; }
+        public required Country Location { get; set; }
+
+        public List<Travel>? travels { get; set; }
+
+        [SetsRequiredMembers]
+        public User(string username, string password, Country location)
+        {
+            Username = username;
+            Password = password;
+            Location = location;
+        }
+    }
+}
