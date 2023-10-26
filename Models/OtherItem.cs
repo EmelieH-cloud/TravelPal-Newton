0c0e87d9afcc252a4cc11aeb30a41f3aa0bf352a
emeliehensfelt <emelie046@gmail.com>
2023-10-26 09:04:26 +0200

    lagt till otherItem klassen

--- Models/OtherItem.cs
+++ Models/OtherItem.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TravelPal_Newton.Interfaces;
 
 namespace TravelPal_Newton.Models
 {
-    internal class OtherItem
+    public class OtherItem : PackingListItem
     {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+
+        public OtherItem(string name, int quantity)
+        {
+            Name = name;
+            Quantity = quantity;
+        }
+        public string GetInfo()
+        {
+            return $"{Name} quantity: {Quantity}";
+        }
     }
 }
