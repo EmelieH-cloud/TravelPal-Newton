1c01bc3c9e62d1dd9ae0e297f4d8efed19b2d0e7
emeliehensfelt <emelie046@gmail.com>
2023-10-26 12:26:29 +0200

    lagt till emptyornull metod till addtravelwindow

--- Windows/AddTravelWindow.xaml.cs
+++ Windows/AddTravelWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
+using System.Windows.Controls;
 using TravelPal_Newton.Enums;
 
 namespace TravelPal_Newton.Windows
@@ -9,6 +11,7 @@ namespace TravelPal_Newton.Windows
     /// </summary>
     public partial class AddTravelWindow : Window
     {
+        bool allFieldsAreFilledin = false;
         public AddTravelWindow()
         {
             InitializeComponent();
@@ -19,7 +22,83 @@ namespace TravelPal_Newton.Windows
                 ComboTravelCountry.Items.Add(country);
             }
 
+        }
+
+        private void BtnOK_Click(object sender, RoutedEventArgs e)
+        {
+
+            // om ett index är valt...
+            if (ComboTravelType.SelectedIndex > -1)
+            {
+                // hämta detta comboBoxItem
+                ComboBoxItem selected = (ComboBoxItem)ComboTravelType.SelectedItem;
+                //.. och hämta sedan dess content som en string. 
+                string content = (string)selected.Content;
+
+                switch (content)
+                {
+                    case "Vacation":
+                        checkInclusive.Visibility = Visibility.Visible;
+                        MessageBox.Show("Please fill in the 'All Inclusive'-box according to your wish.");
+                        break;
+                    case "Worktrip":
+                        lblmdetails.Visibility = Visibility.Visible;
+                        txtMeetingDetails.Visibility = Visibility.Visible;
+                        MessageBox.Show("(Optional) Please fill in the meeting details as you wish.");
+                        break;
+                }
+
+            }
 
         }
+
+        // knappen kommer bara vara synbar efter att användaren valt traveltype. 
+        private void btnfinish_Click(object sender, RoutedEventArgs e)
+        {
+
+            string destination = txtDestination.Text;
+            string startdate = txtStartDate.Text;
+            string enddate = txtEndDate.Text;
+            string travelers = txtTravelers.Text;
+
+            List<String> input = new();
+            input.Add(destination);
+            input.Add(startdate);
+            input.Add(enddate);
+            input.Add(travelers);
+            int counter = 0;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                bool isNullorEmpty = string.IsNullOrEmpty(input[i]);
+                if (!isNullorEmpty)
+                {
+                    counter++;
+                }
+            }
+
+            if (counter == 3) // kommer vara 3 om ingen av dessa strings är null/empty. 
+                allFieldsAreFilledin = true;
+
+            // gå ej vidare om alla fält inte är ifyllda. 
+            if (!allFieldsAreFilledin) { MessageBox.Show("Please fill in all fields"); }
+
+            // Alla fält är ifyllda, och travel är en vacation 
+            else if (allFieldsAreFilledin && checkInclusive.Visibility == Visibility.Visible)
+            {
+                Country country = (Country)ComboTravelCountry.SelectedItem;
+
+                // checkboxen är ifylld. 
+                if (checkInclusive.IsChecked != null)
+                {
+
+                }
+            }
+
+        }
+        //-----------------------------------------------------------------------------
+
+
     }
+
 }
