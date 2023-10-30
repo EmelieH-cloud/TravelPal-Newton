9c42480101968e7836b73ca1567f3337949974d7
emeliehensfelt <emelie046@gmail.com>
2023-10-30 16:43:43 +0100

    design

--- Windows/AboutUs.xaml.cs
+++ Windows/AboutUs.xaml.cs
@@ -1,16 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace TravelPal_Newton.Windows
 {
@@ -22,6 +10,11 @@ namespace TravelPal_Newton.Windows
         public AboutUs()
         {
             InitializeComponent();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
