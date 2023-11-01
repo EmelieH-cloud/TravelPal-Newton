8656fdbd622503bf0fa24b94f9babae25c00781c
emeliehensfelt <emelie046@gmail.com>
2023-11-01 20:22:44 +0100

    lagt till infowindow

---
+++ Windows/InfoWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace TravelPal_Newton.Windows
+{
+    /// <summary>
+    /// Interaction logic for InfoWindow.xaml
+    /// </summary>
+    public partial class InfoWindow : Window
+    {
+        public InfoWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+    }
+}
